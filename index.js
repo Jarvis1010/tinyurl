7ec756ab40e221819ac6760a06158d2f049244fb
Justin Travis Mair <travis.mair@gmail.com>
2016-11-15 07:07:25 +0000

    working version

--- index.js
+++ index.js
@@ -2,26 +2,47 @@ var express=require('express');
 var app=express();
 var mongo=require('mongodb').MongoClient;
 var bodyParser=require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var urlencod=bodyParser.urlencoded();
 var port = process.env.PORT||8080;
 
-app.get('/new/:url',function(req,res){
+app.get('/', function(req,res){
+    res.send("To use this API, use the follwing format at end of above URL '/new/<fullURL>' and JSON of your header info.");
+});
+
+app.get('/new/:url*',function(req,res){
     var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
     var regex = new RegExp(expression);
-    var url=req.params.url;
+    var url=req.url.slice(5);
     if(url.match(regex)){
         var dbURL="mongodb://localhost:27017/tinyurl";
         mongo.connect(dbURL,function(err,db){
-            if(err){
-                console.log("no connection",err)
-            }else{
-                var collection =db.collection('urls');
-                collection.insert({});
-                res.send('regex worked');
-            } 
-        }); 
+            if(!err){
+                db.collection('urls').find({'url':url}).toArray(function(err,result){
+                    if(result.length>0){
+                        var response = {"original_url":url,"short_url":""};
+                        response['short_url']=req.protocol+"://"+req.hostname+"/"+result[0].tiny;
+                        res.send(response);
+                    }else{
+                        db.collection('urls').find().sort({tiny:-1}).limit(1).toArray(function(err,result){
+                            
+                            if(!err){
+                                var tiny=parseInt(result[0].tiny)+1;
+                                var response = {"original_url":url,"short_url":""};
+                                response['short_url']=req.protocol+"://"+req.hostname+"/"+tiny;
+                                db.collection('urls').insert({'url':url,'tiny':tiny});
+                                res.send(response);
+                                db.close();
+                            }
+                            
+                        });
+                    }
+                });
+            }
+         
+        });
     }else{
         res.send({"error":"Wrong url format"});
     }
@@ -50,4 +71,6 @@ app.get("/:url",function(req,res){
     });
 });
 
-app.listen(port);
+app.listen(port, function(){
+    console.log("Server Started");
+});
